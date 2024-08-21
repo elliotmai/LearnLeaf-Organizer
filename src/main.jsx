d8c1f9ad3973ed6f1f1582af71d2ee2998403ba2
Elliot Mai <eli.mai12932@gmail.com>
2024-08-21 16:22:40 -0500

    Update deployment link

--- src/main.jsx
+++ src/main.jsx
@@ -34,7 +34,7 @@ const router = createBrowserRouter(
       ],
     },
   ],
-  { basename: "/LearnLeaf" }
+  { basename: "/LearnLeaf-Organizer" }
 );
 
 ReactDOM.createRoot(document.getElementById('root')).render(
--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-base: "/LearnLeaf"
+base: "/LearnLeaf-Organizer"
 export default defineConfig({
   plugins: [react()],
 })
