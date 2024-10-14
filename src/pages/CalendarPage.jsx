e39867d1d5620ed7c1a64572c53ab7a83f1f19c5
Elliot Mai <eli.mai12932@gmail.com>
2024-10-13 19:40:51 -0500

    add task via fab in calendar

--- src/pages/CalendarPage.jsx
+++ src/pages/CalendarPage.jsx
@@ -1,16 +1,28 @@
 import logo from '/src/LearnLeaf_Name_Logo_Wide.png';
 import React, { useState, useEffect } from 'react';
+import format from 'date-fns/format';
 import CalendarUI from '/src/Components/CalendarPage/CalendarUI';
-import { fetchTasks, logoutUser } from '/src/LearnLeaf_Functions.jsx'
+import { fetchTasks, logoutUser, formatDate } from '/src/LearnLeaf_Functions.jsx'
 import { useUser } from '/src/UserState.jsx';
+import { AddTaskForm } from '/src/Components/TaskView/AddTaskForm.jsx';
 import { useNavigate } from 'react-router-dom';
 import TopBar from '/src/pages/TopBar.jsx';
 import 'react-big-calendar/lib/css/react-big-calendar.css';
-
+import '/src/Components/PageFormat.css';
 
 const CalendarView = () => {
     const { user, updateUser } = useUser();
     const [events, setEvents] = useState([]);
+    const [openAddTask, setOpenAddTask] = useState(false);
+    const [currentDate, setCurrentDate] = useState(formatDate(new Date()));
+
+    useEffect(() => {
+        const intervalId = setInterval(() => {
+          setCurrentDate(formatDate(new Date()));
+        }, 1000); // Update every second
+    
+        return () => clearInterval(intervalId);
+      }, []);
 
     const refreshTasks = async () => {
         if (user && user.id) {
@@ -32,20 +44,30 @@ const CalendarView = () => {
         refreshTasks();
     }, [user?.id]);
 
-    const handleLogout = async () => {
-        try {
-            await logoutUser();
-            updateUser(null);
-            navigate('/');
-        } catch (error) {
-            console.error('Logout failed:', error);
-        }
+    const toggleFormVisibility = () => {
+        console.log(currentDate);
+        setOpenAddTask(!openAddTask);
+    };
+
+    const handleCloseAddTask = () => {
+        setOpenAddTask(false);
     };
 
     return (
         <div className="view-container">
             <TopBar />
+            <button className="fab" onClick={toggleFormVisibility}>
+                +
+            </button>
             <CalendarUI events={events} refreshTasks={refreshTasks}/>
+            {openAddTask && (
+                <AddTaskForm
+                    isOpen={openAddTask}
+                    onClose={handleCloseAddTask}
+                    refreshTasks={refreshTasks}
+                    initialDueDate={currentDate}
+                />
+            )}
         </div>
     );
 }
