73957881a4fe614a28b0ae22ca030d1957e884fa
Elliot Mai <eli.mai12932@gmail.com>
2024-11-14 21:21:51 -0600

    resolve deploy issues

--- vite.config.js
+++ vite.config.js
@@ -15,7 +15,6 @@ function removeUseClientDirective() {
   }
 }
 
-// Vite configuration
 export default defineConfig({
   plugins: [
     react(),
@@ -27,38 +26,55 @@ export default defineConfig({
         short_name: 'LearnLeaf Organizer',
         theme_color: '#ffffff',
         icons: [
-          { src: 'pwa-64x64.png', sizes: '64x64', type: 'image/png' },
-          { src: 'pwa-192x192.png', sizes: '192x192', type: 'image/png' },
-          { src: 'pwa-512x512.png', sizes: '512x512', type: 'image/png', purpose: 'any' },
-          { src: 'maskable-icon-512x512.png', sizes: '512x512', type: 'image/png', purpose: 'maskable' }
+          {
+            src: 'pwa-64x64.png',
+            sizes: '64x64',
+            type: 'image/png'
+          },
+          {
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any'
+          },
+          {
+            src: 'maskable-icon-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'maskable'
+          }
         ],
       },
-      workbox: { maximumFileSizeToCacheInBytes: 5 * 1024 * 1024 },
+      workbox: {
+        maximumFileSizeToCacheInBytes: 5 * 1024 * 1024, // Set cache limit to 5 MB
+      },
     }),
-    removeUseClientDirective() // Removes "use client" directive from MUI files
+    removeUseClientDirective() 
   ],
+  optimizeDeps: {
+    include: ['@mui/material', '@mui/icons-material', 'hoist-non-react-statics'],
+  },
+  build: {
+    rollupOptions: {
+      external: ['@mui/material', '@mui/icons-material'],
+    },
+  },
   server: {
     proxy: {
       '/proxy': {
-        target: 'https://uta.instructure.com',  // Placeholder target
+        target: 'https://uta.instructure.com',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/proxy\/(https?:\/\/[^\/]+)(\/.*)/, '$2'),
         router: (req) => {
           const matches = req.url.match(/^\/proxy\/(https?:\/\/[^\/]+)(\/.*)/);
-          return matches && matches[1] ? matches[1] : 'https://instructure.com';
+          return matches ? matches[1] : 'https://instructure.com';
         },
       },
-    },
-  },
-  optimizeDeps: {
-    exclude: ['@mui/material', '@mui/icons-material'], // Exclude from optimization
-  },
-  build: {
-    rollupOptions: {
-      external: ['@mui/material', '@mui/icons-material'], // Exclude MUI packages from bundling
-    },
-    commonjsOptions: {
-      include: [/node_modules/], // Include all node modules in commonjs plugin handling
     },
   },
 })
