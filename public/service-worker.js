d7b45a328c3da68b5dca89cbab22257abfef0eea
Elliot Mai <eli.mai12932@gmail.com>
2025-03-23 16:06:00 -0500

    test

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,23 +1,24 @@
+// Auto updates
 self.addEventListener('install', (event) => {
     console.log('[Service Worker] Installing new version...');
     event.waitUntil(self.skipWaiting()); // Skip waiting and immediately activate
-  });
-  
-  self.addEventListener('activate', (event) => {
+});
+
+self.addEventListener('activate', (event) => {
     console.log('[Service Worker] Activating...');
     event.waitUntil(self.clients.claim()); // Take control of all pages
-  });
-  
-  self.addEventListener('fetch', (event) => {
+});
+
+// Stay logged in?
+self.addEventListener('fetch', (event) => {
     event.respondWith(
-      caches.open('learnleaf-organizer-cache').then((cache) => {
-        return fetch(event.request)
-          .then((response) => {
-            cache.put(event.request, response.clone());
-            return response;
-          })
-          .catch(() => cache.match(event.request));
-      })
+        caches.open('learnleaf-organizer-cache').then((cache) => {
+            return fetch(event.request)
+                .then((response) => {
+                    cache.put(event.request, response.clone());
+                    return response;
+                })
+                .catch(() => cache.match(event.request));
+        })
     );
-  });
-  
+});
